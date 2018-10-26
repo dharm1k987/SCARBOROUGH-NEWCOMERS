da579c0073938a1636d37f278f4530735c0351b8
caleb1111 <calebchen1997@gmail.com>
2018-10-26 19:29:19 -0400

    Change the redirection to the home page after login

--- src/public/js/logout.js
+++ src/public/js/logout.js
@@ -1,4 +1,10 @@
 $(document).ready(function() {
+    if (localStorage.loginOrg == "false" && localStorage.loginTEQ == "false") {
+        window.location.replace("/login");
+        // alert("Please login first");
+        return;
+    }
+    
     $("#logout-btn").click(function() {
         localStorage.setItem("loginOrg", "false"); localStorage.setItem("loginTEQ", "false");
         window.location.replace("/login");
