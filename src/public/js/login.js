15264c1ce7ed2077eb8241f86425c65fa6549664
caleb1111 <calebchen1997@gmail.com>
2018-10-26 17:25:13 -0400

    Undo the changes in login.js

--- src/public/js/login.js
+++ src/public/js/login.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
     // if we are already logged in, we cannot be on the login page
     if (localStorage.loginOrg == "true") {
         console.log("already logged in, redirect to org");
-        window.location.replace("/home");
+        window.location.replace("/upload");
     } else if (localStorage.loginTEQ == "true") {
         console.log("already logged in, redirect to teq")
     }
@@ -22,7 +22,7 @@ $(document).ready(function() {
                     success: function(response2) {
                         if (response2.type == "org") {
                             localStorage.setItem("loginOrg", "true"); localStorage.setItem("loginTEQ", "false");
-                            window.location.replace("/home");
+                            window.location.replace("/upload");
                         } else {
                             localStorage.setItem("loginOrg", "true"); localStorage.setItem("loginTEQ", "false");
                             console.log("assuming teq");
