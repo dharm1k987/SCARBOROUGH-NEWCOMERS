0140d39c56e5169618c026a7f6fefd6b92833c64
= <dharm1k987@gmail.com>
2018-10-27 15:27:58 -0400

    db works

--- src/public/controllers/loginController.js
+++ src/public/controllers/loginController.js
@@ -1,4 +1,6 @@
 var bodyParser = require("body-parser");
+var Datastore = require('nedb');
+var db = new Datastore({filename: 'src/public/db/account.db', autoload: true});
 var urlencodedParser = bodyParser.urlencoded({extended: false});
 
 module.exports  = function(app) {
@@ -9,8 +11,42 @@ module.exports  = function(app) {
         res.render("login");
     });
 
+    app.post("/fill", urlencodedParser, function(req, res) {
+        db.insert( {"username": "test1", "password": "testpass", "type": "org"});
+        res.status(200);
+        res.send();
+    });
+
+    app.get("/fill", urlencodedParser, function(req, res) {
+
+        db.find({"username": "test1", "password": "testpass"}, function(err, docs){
+            if (err) {
+                res.status(400);
+                res.send();
+            } else {
+                res.status(200);
+                console.log(docs);
+                var result = docs[0].type;
+                res.send({type: result});
+            }
+        });
+    });
+    
+
     app.post("/login", urlencodedParser, function(req, res) {
         // temp for testing
+
+        db.find({"username": req.body.username, "password": req.body.password}, function(err, docs){
+            if (err) {
+                res.status(400);
+                res.send();
+            } else {
+                res.status(200);
+                var result = docs[0].type;
+                res.send({type: result});
+            }
+        });
+        /*
         var username = "username";
         var password = "password";
 
@@ -22,6 +58,6 @@ module.exports  = function(app) {
             console.log("sending 400");
             res.status(400);
             res.send();
-        }
+        }*/
     });
 };
