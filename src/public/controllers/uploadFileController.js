83810f79192aaf706f8b80bb4d0b5b535f0135bf
Byron Leung <byron.leung@mail.utoronto.ca>
2018-10-27 22:10:45 -0400

    Implement receiving file and storing it cache

--- src/public/controllers/uploadFileController.js
+++ src/public/controllers/uploadFileController.js
@@ -1,10 +1,15 @@
-var bodyParser = require("body-parser");
+var multer = require('multer');
+var storage = multer.memoryStorage();
+var upload = multer({ storage: storage });
 
 module.exports  = function(app) {
 
     app.get("/upload", function(req, res) {
         console.log("this page should only be avialble to the members of supporting agencies is logged in... watch for that");
         res.render("uploading-page");
-    })
+    });
 
+    app.post('/upload', upload.single('csv'), function(req, res) {
+        // file is cached in req.file.buffer
+    });
 };
