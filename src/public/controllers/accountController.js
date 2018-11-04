2e422ad570aabfe30c8f1dfb8a1ebbf0cdb4dbed
= <dharm1k987@gmail.com>
2018-11-04 18:27:23 -0500

    changed all find to docs.length ==0 and add delete acc endpoint'
    exit

--- src/public/controllers/accountController.js
+++ src/public/controllers/accountController.js
@@ -5,14 +5,10 @@ var urlencodedParser = bodyParser.urlencoded({extended: false});
 
 module.exports  = function(app) {
 
-    app.get("/account", function(req, res) {
-        console.log("this page should only be avialble the teq staff is logged in... watch for that");
-        res.render("create-org-account");
-    })
 
     app.get("/account/type", urlencodedParser, function(req, res) {
         db.find({"username": req.query.username, "password": req.query.password}, function(err, docs){
-            if (err) {
+            if (docs.length == 0) {
                 res.status(400);
                 res.send();
             } else {
@@ -20,6 +16,28 @@ module.exports  = function(app) {
                  console.log(docs);
                 var result = docs[0].type;
                 res.send({type: result});
+            }
+        });
+    });
+
+
+    app.post("/account/delete", urlencodedParser, function(req, res) {
+        console.log("inside");
+        var username = req.body.username;
+        var password = req.body.password;
+
+        db.find({ username: username }, function (err, docs) {
+            if (docs.length != 0 && docs[0]["password"] == password) {
+                console.log("found something");
+                db.remove({ username: username }, function (err, numRemoved) {
+                    res.status(200);
+                    res.send()
+                });
+            } else {
+                console.log("didnt find");
+                res.status(400);
+                res.send()
+                
             }
         });
     });
