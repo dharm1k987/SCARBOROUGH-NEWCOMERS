344e69590b5103390619425f6ebde38a0cbbd219
ralph-dev <ralph.maamari@mail.utoronto.ca>
2018-11-04 00:51:52 -0400

    configured npm start and npm test

--- test/test.js
+++ test/test.js
@@ -1,11 +1,21 @@
 // https://mochajs.org
+// Run npm start to get the server running then run npm test.
 var assert = require('assert');
+var axios = require('axios');
 
-
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal([1,2,3].indexOf(4), -1);
+describe('Create Account and Login', function() {
+  it('#create endpoint', function(done) {
+    var data = {
+      "username": "Carl",
+      "password": "Bryon",
+      "type": "TEQ"
+    }
+    axios.post('http://localhost:8080/create', data)
+    .then(function (response) {
+      done()
+    })
+    .catch(function (error) {
+      done(error)
     });
   });
 });
