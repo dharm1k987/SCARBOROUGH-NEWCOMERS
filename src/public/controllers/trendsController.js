adc0b654c7a892741ad07e05bb2592927fe9ee65
leungbyr <byron.leung@mail.utoronto.ca>
2018-11-15 10:29:26 -0500

    Create page and json object for generating trends

---
+++ src/public/controllers/trendsController.js
@@ -0,0 +1,55 @@
+var bodyParser = require("body-parser");
+var urlencodedParser = bodyParser.urlencoded({extended: false});
+var index = require(__dirname + '/../../index');
+var db2 = index.db2;
+
+function generateJson (docs, cb) {
+    var res = {};
+
+    for (i in docs) {
+        var month = docs[i]['month'];
+        var entries = docs[i]['entries'];
+        res[month] = {'clients': entries.length, 'services': 0};
+
+        for (j in entries) {
+            var entry = entries[j];
+            var input;
+
+            if (typeof entry['Support Services Received'] !== 'undefined') {
+                input = entry['Support Services Received'];
+            } else if (typeof entry['Support services received'] !== 'undefined') {
+                input = entry['Support services received'];
+            }
+
+            if (input === 'Yes') {
+                res[month]['services']++;
+            }
+        }
+    }
+
+    cb(res);
+}
+
+module.exports = function (app) {
+    app.get('/trends', function (req, res) {
+        res.render('trends');
+    });
+
+    app.post('/generate-trends', urlencodedParser, function (req, res) {
+        db2.find({template: req.body.template}, function (err, docs) {
+            if (err) {
+                res.send(500);
+            } else {
+                if (docs.length !== 0) {
+                    generateJson(docs, function (response) {
+                        res.status(200);
+                        res.json(response);
+                    });
+                } else {
+                    res.status(200);
+                    res.json({});
+                }
+            }
+        });
+    });
+}
---
+++ src/public/js/trends.js
@@ -0,0 +1,76 @@
+$(document).ready(function() {
+    if (localStorage.loginOrg == "false" && localStorage.loginTEQ == "false") {
+        window.location.replace("/login");
+        // alert("Please login first");
+        return;
+    } else if (localStorage.loginOrg == "true" && localStorage.loginTEQ == "false") {
+        window.location.replace("/home-org");
+        // alert("Please login first");
+        return;
+    }
+
+    console.log("in generate");
+
+    // get the months and add them to the dropdown
+    months = [];
+    $.ajax({
+        type: 'GET',
+        url: '/generate/months',
+        data: {},
+        success: function(response) {
+            // get the type of account they are (teq or org)
+            // setup local storage
+            console.log("success - months:");
+            for (var template in response) {
+                // we might have many months
+                for (var eachMonth in response[template]) {
+                    console.log("inner");
+                    var result = response[template][eachMonth];
+                    // only add if its not already inside
+                    if (months.indexOf(result) < 0) {
+                        console.log(result + " not in mon");
+                        months.push(result);
+                        $("#monthID").append($('<option></option>').val(result).html(result));
+                        console.log(months);
+                    }
+
+                }
+            }
+            //console.log(months);
+
+            // add it to dropdown
+            
+        },
+        error: function(response) {
+            console.log("something not right.");
+        }
+    });
+
+
+
+    $("#generate-btn").click(function() {
+        var e = document.getElementById("ddlViewBy");
+        var option = e.options[e.selectedIndex].value;
+        var f = document.getElementById("monthID");
+        var option2 = f.options[f.selectedIndex].value;
+
+        var data = {template: option, date: option2};
+        $.ajax({
+            type: 'POST',
+            url: '/generate-trends',
+            data: data,
+            success: function(response) {
+                // get the type of account they are (teq or org)
+                // setup local storage
+                console.log("success");
+                console.log(response);
+
+                // reroute them based on the location data provides
+            },
+            error: function(response) {
+                console.log("something not right.");
+            }
+        });
+    });
+
+});
